41035266c01ea532434ee79d0490547b68178d73
Şevval Dilara Biçer <41507884+sevvalbicerr@users.noreply.github.com>
2023-03-27 11:31:52 +0300

    Global Exception Handler added

--- NLayer.Service/Services/Services.cs
+++ NLayer.Service/Services/Services.cs
@@ -2,6 +2,7 @@
 using NLayer.Core.Repositories;
 using NLayer.Core.Services;
 using NLayer.Core.UnitOfWorks;
+using NLayer.Service.Exceptions;
 using System.Linq.Expressions;
 
 namespace NLayer.Service.Services
@@ -55,9 +56,14 @@ namespace NLayer.Service.Services
            return await _repository.GetAll().ToListAsync();
         }
 
-        public Task<T> GetByIdAsync(int id)
+        public async Task<T> GetByIdAsync(int id)
         {
-            return _repository.GetByIdAsync(id);
+            var hasProduct=await _repository.GetByIdAsync(id);
+            if (hasProduct == null)
+            {
+                throw new NotFoundException($"{typeof(T).Name} not found");
+            }
+            return hasProduct;
         }
 
         public async Task RemoveAsync(T entity)
